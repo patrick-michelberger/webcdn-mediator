0f70b7c99a8c76cf57a0cfee5db514150db835aa
patrick_michelberger <pmichelberger@gmx.de>
2015-08-05 11:42:24 +0200

    boilerplate content removed

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,7 +3,7 @@
 angular.module('webcdnMediatorApp')
   .controller('NavbarCtrl', function ($scope, $location) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'statistics',
       'link': '/'
     }];
 
