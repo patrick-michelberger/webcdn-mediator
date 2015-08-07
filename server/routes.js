5728df976cf9203bf728ee9a371a233c04d3d561
patrick_michelberger <pmichelberger@gmx.de>
2015-08-07 11:02:19 +0200

    CORS settings added

--- server/routes.js
+++ server/routes.js
@@ -8,17 +8,24 @@ var errors = require('./components/errors');
 
 module.exports = function(app) {
 
-  // Insert routes below
-  app.use('/api/hosts', require('./api/host'));
-  app.use('/api/things', require('./api/thing'));
-  
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
-
-  // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/index.html');
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Methods', 'GET,DELETE,POST');
+        next();
     });
+
+    // Insert routes below
+    app.use('/api/hosts', require('./api/host'));
+    app.use('/api/things', require('./api/thing'));
+
+    // All undefined asset or api routes should return a 404
+    app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+        .get(errors[404]);
+
+    // All other routes should redirect to the index.html
+    app.route('/*')
+        .get(function(req, res) {
+            res.sendfile(app.get('appPath') + '/index.html');
+        });
 };
