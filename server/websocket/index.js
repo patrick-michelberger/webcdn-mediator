c2af0e345de21803c90879fb45b1861bf7484b06
patrick_michelberger <pmichelberger@gmx.de>
2015-08-09 16:56:11 +0200

    add / remove host functionality added

--- server/websocket/index.js
+++ server/websocket/index.js
@@ -11,11 +11,16 @@ var http = require('http');
  * Initialize `Server`
  * @api private
  */
+
+var Host = require('../api/host/host.model');
+
 var Server = function() {
+    this.sockets = {};
     this.init();
 };
 
 Server.prototype.init = function() {
+	var self = this;
 
     // HTTP SERVER
     var server = http.createServer(function(request, response) {});
@@ -32,9 +37,81 @@ Server.prototype.init = function() {
     wsServer.on('request', function(request) {
         console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
         var connection = request.accept(null, request.origin);
+
+        if (request.resourceURL && request.resourceURL.query) {
+            // parse query information
+
+            if (request.resourceURL.query.id) {
+                // peer ID received
+                var peer_id = request.resourceURL.query.id;
+                connection.peerid = peer_id;
+                self.sockets[peer_id] = connection;
+
+            }
+
+            connection.on('message', function(message) {
+                if (message && message.utf8Data) {
+                    var msg = JSON.parse(message.utf8Data);
+                    var type = msg.type;
+                    var data = msg.data;
+                    switch (type) {
+                        case "host:add":
+                            addHost(data);
+                            break;
+                        case "host:remove":
+                            removeHost(data.uuid);
+                            break;
+                    }
+                }
+            });
+
+            connection.on('close', function(reasonCode, description) {
+                // close user connection
+                console.log((new Date()) + " Peer disconnected.");
+                var connectionDeleted = false;
+                for (var key in self.sockets) {
+                    if (self.sockets[key] === connection) {
+                    	removeHost(self.sockets[key].peerid);
+                        delete self.sockets[key];
+                        connectionDeleted = true;
+                    }
+                }
+                if (!connectionDeleted) {
+                    console.log("Delete disconnected peer fail");
+                }
+            });
+
+        }
     });
 
     this._wsServer = wsServer;
+};
+
+var removeHost = function(uuid) {
+    Host.findOne({
+        uuid: uuid
+    }, function(err, host) {
+        if (err) {
+            console.log("err: ", err);
+        }
+        if (host) {
+            host.remove(function(err) {
+                if (err) {
+                    console.log("err: ", err);
+                }
+                console.log("Host removed: " + uuid);
+            });
+        }
+    });
+};
+
+var addHost = function(data) {
+    Host.create(data, function(err, host) {
+        if (err) {
+            console.log("err: ", err);
+        }
+        console.log("Host created: " + data.uuid);
+    });
 };
 
 /**
