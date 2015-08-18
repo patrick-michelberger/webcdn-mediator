101cf3025e2e9f4a74710f4105c05f034e7cb56c
patrick_michelberger <pmichelberger@gmx.de>
2015-08-18 16:45:05 +0200

    created field added to host

--- server/api/host/host.model.js
+++ server/api/host/host.model.js
@@ -4,17 +4,21 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var HostSchema = new Schema({
-  "uuid": String,
-  "active": {
-  	"type": Boolean, 
-  	"default": false
-  },
-  "performance": {},
-  "resource_timing": [],
-  "pc_connect_duration": [],
-  "lookup_duration": [],
-  "fetch_duration": [],
-  "sendImage_duration": []
+    "uuid": String,
+    "active": {
+        "type": Boolean,
+        "default": false
+    },
+    "performance": {},
+    "resource_timing": [],
+    "pc_connect_duration": [],
+    "lookup_duration": [],
+    "fetch_duration": [],
+    "sendImage_duration": [],
+    "created": {
+        "type": Date,
+        "default": Date.now
+    }
 });
 
 module.exports = mongoose.model('Host', HostSchema);
